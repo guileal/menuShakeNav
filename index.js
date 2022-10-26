c1d6a606a17c6592c1d433cdf8a1d745f73c94c4
Guilherme Leal <guileal.designer@gmail.com>
2022-10-26 05:35:39 -0300

    insert express middleware to serve static files / response with .html

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
+
+app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
-    res.send('hello')
-    console.log(__dirname)
+    res.sendFile(__dirname + "/index.html")
 })
 
 app.listen(port, () => {
